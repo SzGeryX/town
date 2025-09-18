89444637e14c3d5178e158a469df5c0db65aa5ab
Szentjóbi Gergő <szentjobi.gergo@mechwart.local>
2025-09-18 13:57:01 +0200

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{   class Town
+    {
+        string name;
+        int population;
+        bool charging;
+        Vector2 cords;
+
+        Town(string line)
+        {
+            string[] data = line.Split(';');
+
+            this.name = data[0];
+            this.population = int.Parse(data[1]);
+            this.charging = Convert.ToBoolean(int.Parse(data[2]));
+            this.cords = new Vector2(float.Parse(data[3]), float.Parse(data[4]));
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Numerics;
+using System.Security.Cryptography;
+
+internal class Program
+{ 
+    
+    private static void Main(string[] args)
+    {
+       
+    }
+}
